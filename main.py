4e12e8fa46169f0887b4d56acc78e4f0c8e1ea52
vivalkm <vivalkm@gmail.com>
2022-08-20 01:31:51 -0700

    update column ordering by difficulty

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ session = requests.Session()
 requests.utils.add_dict_to_cookiejar(session.cookies, my_cookies)
 
 # Retrieve data
+
+
 def getData(days=0):
     df_submissions = pd.DataFrame()
     has_next = True
@@ -19,12 +21,15 @@ def getData(days=0):
     break_flag = False
     try:
         while has_next and not break_flag:
-            response = session.post(url+"/graphql", json=settings.SubmissionQuery(offset))
+            response = session.post(
+                url+"/graphql", json=settings.SubmissionQuery(offset))
             submission_data = json.loads(response.content.decode('utf-8'))
             for submission in submission_data["data"]["submissionList"]["submissions"]:
                 if submission["statusDisplay"] == "Accepted":
-                    df_submissions = df_submissions.append(submission, ignore_index=True)
-                    date_set.add(datetime.fromtimestamp(int(submission["timestamp"])).strftime("%Y-%m-%d"))
+                    df_submissions = df_submissions.append(
+                        submission, ignore_index=True)
+                    date_set.add(datetime.fromtimestamp(
+                        int(submission["timestamp"])).strftime("%Y-%m-%d"))
                     if days > 0 and len(date_set) > days:
                         break_flag = True
                         break
@@ -37,12 +42,15 @@ def getData(days=0):
     return df_submissions
 
 # Join with difficulty
+
+
 def mapDifficulty(df_submissions):
     if len(df_submissions) > 0:
         df_submissions["Date"] = (pd.to_datetime(df_submissions["timestamp"], unit='s', utc=True)
                                     .dt.tz_convert('US/Pacific')
                                     .dt.strftime("%Y-%m-%d"))
-        df_submissions.drop(["timestamp", "statusDisplay"], axis=1, inplace=True)
+        df_submissions.drop(["timestamp", "statusDisplay"],
+                            axis=1, inplace=True)
         df_submissions = df_submissions.drop_duplicates().reset_index(drop=True)
         # Add difficulty
         difficulty = []
@@ -50,30 +58,50 @@ def mapDifficulty(df_submissions):
             if i > 0 and df_submissions.loc[i, "titleSlug"] == df_submissions.loc[i-1, "titleSlug"]:
                 difficulty.append(difficulty[-1])
             else:
-                response = session.post(url+"/graphql", json=settings.QuestionQuery(df_submissions.loc[i, "titleSlug"]))
+                response = session.post(
+                    url+"/graphql", json=settings.QuestionQuery(df_submissions.loc[i, "titleSlug"]))
                 question_data = json.loads(response.content.decode('utf-8'))
-                difficulty.append(question_data["data"]["question"]["difficulty"])
+                
+                # get difficulty
+                d = question_data["data"]["question"]["difficulty"]
+                
+                # rename difficulty for column ordering
+                if d == 'Easy':
+                    d = '1_Easy'
+                elif d == 'Medium':
+                    d = '2_Medium'
+                else:
+                    d = '3_Hard'
+                difficulty.append(d)
         df_submissions['Difficulty'] = difficulty
     return df_submissions
 
 # Organize and present data
+
+
 def pivot(df_submissions):
     if len(df_submissions) > 0:
-        df_summary = df_submissions.pivot_table(index="Date", columns="Difficulty", values="titleSlug", aggfunc='count', fill_value=0)
+        df_summary = df_submissions.pivot_table(
+            index="Date", columns="Difficulty", values="titleSlug", aggfunc='count', fill_value=0)
         df_summary.insert(0, "Total", df_summary.sum(axis=1))
         return df_summary
     else:
-        return -1
+        return df_submissions
 
 # Pull all accepted records by date
+
+
 def allSubmission():
     df_submissions = getData()
     print(pivot(df_submissions))
 
 # Pull recent n days of accepted records by date
+
+
 def recentNDaySubmission(days):
     df_submissions = getData(days)
     print(pivot(df_submissions).tail(days))
+
 
 if __name__ == '__main__':
     recentNDaySubmission(10)
